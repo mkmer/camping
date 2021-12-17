42ea48e14f77c16435619f81026bc18d60cf2922
Mike Kasper <m_kasper@sbcglobal.net>
2021-12-17 09:44:47 -0500

    Modified date calc

--- Camping.py
+++ Camping.py
@@ -1,3 +1,4 @@
+from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.edge.options import Options as EdgeOptions
 from selenium.webdriver import Edge as Edge
@@ -10,12 +11,19 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import time
 import msvcrt
+import datetime
+import os
 
 # Modify these values for your reservation
-mySites = ["139","125"]
+mySites = ["56"]
+days = 15
 StartDate = "2022-06-17"
-EndDate = "2022-06-29"
-numberTabs = 5
+
+# Calculate the end date
+EndDateDT = datetime.datetime.strptime(StartDate,"%Y-%m-%d") + datetime.timedelta(days=days)
+EndDate = EndDateDT.strftime("%Y-%m-%d")
+#EndDate = "2022-07-02"
+numberTabs = 8
 
 
 
@@ -24,19 +32,19 @@ ReserveButton = "addToStay"
 site = "site-label-text"
 delay = 10 #seconds
 #GrandHaven beachfront
-myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483593&mapId=-2147483479&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
+#myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483593&mapId=-2147483479&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 #GrandHaven back half
 #myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483593&mapId=-2147483480&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 #Pentwater front
 #myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483555&mapId=-2147483276&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 #Pentwater Back
-#myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483555&mapId=-2147483277&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
+myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483555&mapId=-2147483277&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 
 tabs = list(range(1,numberTabs*len(mySites)))
 # Build up the list of tabs 
 
 
-driver = Edge()
+driver = Edge(verbose=False,service_log_path=os.devnull)
 driver.get(myurl)
 
 #wait for consent button
@@ -45,7 +53,7 @@ try:
 except:
     print ("I timed out  looking for consent:(")
 
-time.sleep(2)
+time.sleep(2) 
 action_chains = ActionChains(driver)
 action_chains.move_to_element(consent).click()
 action_chains.pause(1)
@@ -54,7 +62,7 @@ action_chains.perform()
 #here we wait for the site map to load....
 try:
     map = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,site)))
-    time.sleep(1)
+    time.sleep(2)
 except:
     print ("I timed out looking for the site")
 
@@ -80,11 +88,10 @@ for x in tabs:
    driver.switch_to.window(driver.window_handles[(x)])
    driver.get(myurl)
 
-   #time.sleep(10)
-   #Wait for the site map to load
+   #Wait for the site map to load... not quite right but I can't figure out another way....
    try:
        map = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,site)))
-       time.sleep(1)
+       time.sleep(2)
    except:
        print ("I timed out looking for the site")
    
@@ -110,15 +117,18 @@ for x in tabs:
     driver.switch_to.window(driver.window_handles[(x)])
     #time.sleep(1)
     try:
-        reserve = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.ID,ReserveButton)))
+        reserve = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.ID,ReserveButton)))
+        action_chains = ActionChains(driver)
+        action_chains.move_to_element(reserve).click()
+        action_chains.perform()
+
     except:
         print ("I timed out  looking for reserve:(")
 #reserve = driver.find_elements_by_id(ReserveButton)
-    action_chains = ActionChains(driver)
-    action_chains.move_to_element(reserve).click()
-    action_chains.perform()
 
 
+print ("Keep the broswer Open")
+msvcrt.getch()
 
 
 
