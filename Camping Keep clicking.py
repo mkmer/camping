032d7ab6be60bf42554511e92f8809744749999f
Mike Kasper <m_kasper@sbcglobal.net>
2022-01-10 10:31:54 -0500

    Add clicker

--- Camping Keep clicking.py
+++ Camping Keep clicking.py
@@ -15,15 +15,15 @@ import datetime
 import os
 
 # Modify these values for your reservation
-mySites = ["153"]
+mySites = ["151"]
 days = 15
-StartDate = "2022-07-10"
+StartDate = "2022-07-11"
 
 # Calculate the end date
 EndDateDT = datetime.datetime.strptime(StartDate,"%Y-%m-%d") + datetime.timedelta(days=days)
 EndDate = EndDateDT.strftime("%Y-%m-%d")
-#EndDate = "2022-07-02"
-numberTabs = 8
+
+
 
 
 
@@ -40,7 +40,7 @@ myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationI
 #Pentwater Back
 #myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483555&mapId=-2147483277&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 
-tabs = list(range(1,numberTabs*len(mySites)))
+tabs = list(range(1,1))
 # Build up the list of tabs 
 
 
@@ -72,58 +72,47 @@ for theSite in sites: # find the site we're looking for and click on it
     if theSite.text == mySites[0]:
         action_chains = ActionChains(driver)
         action_chains.move_to_element(theSite).click()
-        action_chains.pause(1)
+        #action_chains.pause(1)
         action_chains.perform()
         break
 NumberOfSites = len(mySites)
 
-if len(mySites) > 1:
-   index = 1 # index through the sites we want to try to reserve
-else:
-   index = 0
 
-for x in tabs:
-   driver.execute_script("window.open(arguments[0], \
-                          arguments[1]);",("about:blank",x))
-   driver.switch_to.window(driver.window_handles[(x)])
-   driver.get(myurl)
+#Just open one tab for the "keep clicking"
+#driver.execute_script("window.open(arguments[0], \
+#                        arguments[1]);",("about:blank",x))
+#driver.switch_to.window(driver.window_handles[(x)])
+#driver.get(myurl)
 
-   #Wait for the site map to load... not quite right but I can't figure out another way....
-   try:
-       map = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.CLASS_NAME,site)))
-       time.sleep(2)
-   except:
-       print ("I timed out looking for the site")
-   
-   sites = driver.find_elements_by_class_name(site)
-   for theSite in sites: # find the site we're looking for and click on it
-       if theSite.text == mySites[index]:
-           action_chains = ActionChains(driver)
-           action_chains.move_to_element(theSite).click()
-           action_chains.perform()
-           break
+#Wait for the site map to load... not quite right but I can't figure out another way....
 
-   index = index + 1
-   if index >= len(mySites):
-       index = 0
-
-
-time.sleep(3)
+#time.sleep(3)
 #now find the reserve button
-print ("Press any key to start clicking")
-msvcrt.getch()
-tabs.insert(0,0)
-for x in tabs:
-    driver.switch_to.window(driver.window_handles[(x)])
+print ("start clicking")
+#msvcrt.getch()
+#tabs.insert(0,0)
+while (True):
+    #driver.switch_to.window(driver.window_handles[(x)])
     #time.sleep(1)
     try:
-        reserve = WebDriverWait(driver,1).until(EC.presence_of_element_located((By.ID,ReserveButton)))
+        reserve = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.ID,ReserveButton)))
         action_chains = ActionChains(driver)
         action_chains.move_to_element(reserve).click()
         action_chains.perform()
 
     except:
         print ("I timed out  looking for reserve:(")
+
+    try:
+        close_dialog = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.XPATH,"//button[.='Close']")))
+        action_chains = ActionChains(driver)
+        action_chains.move_to_element(close_dialog).click()
+        #action_chains.pause(1)
+        action_chains.perform()
+
+    except:
+        print ("I timed out  looking for close:(")
+        break
 #reserve = driver.find_elements_by_id(ReserveButton)
 
 
