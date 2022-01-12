10d7f8d13d3321d580e3940ea43a3b5159db09d4
Mike Kasper <m_kasper@sbcglobal.net>
2022-01-12 07:14:48 -0500

    v2

--- Camping Keep clicking.py
+++ Camping Keep clicking.py
@@ -9,14 +9,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+import keyboard
 import time
 import msvcrt
 import datetime
 import os
 
 # Modify these values for your reservation
-mySites = ["151"]
-days = 15
+mySites = ["141"]
+days = 14
 StartDate = "2022-07-11"
 
 # Calculate the end date
@@ -26,7 +27,9 @@ EndDate = EndDateDT.strftime("%Y-%m-%d")
 # general variables
 ReserveButton = "addToStay"
 site = "site-label-text"
+close_button = "//button[.='Close']"
 delay = 10 #seconds
+stop = False
 #GrandHaven beachfront
 myurl = f'https://midnrreservations.com/create-booking/results?resourceLocationId=-2147483593&mapId=-2147483479&searchTabGroupId=0&bookingCategoryId=0&startDate={StartDate}&endDate={EndDate}&isReserving=true&equipmentId=-32768&subEquipmentId=-32765&partySize=4&-32761=%5B%5B1%5D,0,1%5D'
 #GrandHaven back half
@@ -74,22 +77,13 @@ for theSite in sites: # find the site we're looking for and click on it
 NumberOfSites = len(mySites)
 
 
-#Just open one tab for the "keep clicking"
-#driver.execute_script("window.open(arguments[0], \
-#                        arguments[1]);",("about:blank",x))
-#driver.switch_to.window(driver.window_handles[(x)])
-#driver.get(myurl)
 
-#Wait for the site map to load... not quite right but I can't figure out another way....
-
-#time.sleep(3)
 #now find the reserve button
-print ("start clicking")
-#msvcrt.getch()
-#tabs.insert(0,0)
+print("Press a key to start clicking")
+msvcrt.getch()
+print ("started clicking Press a key to stop")
+
 while (True):
-    #driver.switch_to.window(driver.window_handles[(x)])
-    #time.sleep(1)
     try:
         reserve = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.ID,ReserveButton)))
         action_chains = ActionChains(driver)
@@ -100,7 +94,7 @@ while (True):
         print ("I timed out  looking for reserve:(")
 
     try:
-        close_dialog = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.XPATH,"//button[.='Close']")))
+        close_dialog = WebDriverWait(driver,delay).until(EC.presence_of_element_located((By.XPATH,close_button)))
         action_chains = ActionChains(driver)
         action_chains.move_to_element(close_dialog).click()
         #action_chains.pause(1)
@@ -109,10 +103,14 @@ while (True):
     except:
         print ("I timed out  looking for close:(")
         break
-#reserve = driver.find_elements_by_id(ReserveButton)
+    
+    
+    if msvcrt.kbhit():
+        time.sleep(1)
+        break
 
 
-print ("Keep the broswer Open")
+print ("Keep the broswer Open") 
 msvcrt.getch()
 
 
